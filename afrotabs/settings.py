5d6579abe21d5ed84b4eb6f56466deea0428cc77
missionwp <johnmgathenya@gmail.com>
2023-02-01 00:49:40 +0300

    added req

--- afrotabs/settings.py
+++ afrotabs/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = 'var/www/gathenya/static'
+STATIC_ROOT = 'var/www/afrotabs/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
