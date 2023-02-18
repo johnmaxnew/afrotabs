6aad3eb4849bbbe99db807920adf9706ee45ef7c
missionwp <johnmgathenya@gmail.com>
2023-02-19 01:25:48 +0300

    updated settings

--- afrotabs/settings.py
+++ afrotabs/settings.py
@@ -130,7 +130,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = '/var/www/afrotabs/static'
+# STATIC_ROOT = '/var/www/afrotabs/static'
+
+STATIC_ROOT = 'var/www/afrotabs/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
